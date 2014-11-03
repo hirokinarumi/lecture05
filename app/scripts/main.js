954234842cfea747f43d2193f6f437f4ce65e4d4
hirokinarumi <hiroki_imac.pc@icloud.com>
2014-11-03 14:06:02 +0900

    課題
    課題

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,22 @@
+
 var greeting = function(message){
   window.alert(message);
 };
 
 var helloWorld = function(event){
+  var input = document.querySelector("input");
+  var inputText = input.value;
   console.log("helloWorld: start");
   greeting("Hello, world");
   console.log("helloWorld: end");
 };
 
+
 var boot = function(event){
   console.log("boot: start");
-  var button = document.querySelector("button");
+  var button = document.querySelector("h1");
   button.addEventListener("click", helloWorld);
+  button.addEventListener("dblclic",helloworld);
   console.log("boot: end");
 };
 
